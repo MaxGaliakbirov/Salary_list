507fd45237eba049476e0516064b48bc277ba45c
maksimgaliakbirov <mf.galiakbirov@gmail.com>
2023-11-14 00:18:23 +0300

    add read line and function

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,26 @@
-fun main () {
+/*
+------------------------
+|   Max   |   500000   |
+------------------------
+|   Alex  |   250000   |
+------------------------
+*/
 
+fun main () {
+    println("Введите список имён через запятую:")
+    val namesArr = readLine()?.split(",")
+    println("Введите зарплату (без дробей) через запятую:")
+    val salaryArr = readLine()?.split(",")
+    println(table(namesArr, salaryArr))
+}
+fun table (names: List<String>?, salary: List<String>?) {
+    if (names != null && salary != null) {
+        for (i in 0..names.size) {
+            println("-".repeat(24))
+            print("|")
+            print(names[i])
+            print("|")
+            println(salary[i])
+        }
+    }
 }
